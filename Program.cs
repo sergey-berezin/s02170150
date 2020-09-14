dfb1248487ceda39b3b2cc8192703a450a0ca011
n.lokshin <n.lokshin@partner.samsung.com>
2020-09-14 12:05:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace s02170150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
