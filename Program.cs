989b533eaeeeda184ad5a131abe04032d0b0a0a4
n.lokshin <n.lokshin@partner.samsung.com>
2020-09-14 12:12:08 +0300

    Test in SmartGit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace s02170150
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("New change");
         }
     }
 }
