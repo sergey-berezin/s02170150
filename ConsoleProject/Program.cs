c29c0880f7415f4a033eeb34ec5245c336952112
Никита Локшин <lockshin1999@mail.ru>
2020-10-11 21:57:48 +0300

    Task 01 (#1)
    * Initial commit
    
    * Test in SmartGit
    
    * Necessary changes
    
    * Delete .gitignore
    
    * Two projects
    
    * Single image classification
    
    * Directory iteration
    
    * Implemented threads
    
    * Add solution
    
    * Threading fix
    
    * Path fix
    
    * Added events
    
    * Removed console dependencies
    
    Co-authored-by: n.lokshin <n.lokshin@partner.samsung.com>

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using PredictorLibrary;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        public static void Output(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new Predictor(args.FirstOrDefault() ?? "./images/", Output);
+            Console.WriteLine(a.ToString());
+            Console.CancelKeyPress += (sender, eArgs) => {
+                a.Stop();
+                eArgs.Cancel = true;
+            };
+            a.ProcessDirectory();
+        }
+    }
+}
