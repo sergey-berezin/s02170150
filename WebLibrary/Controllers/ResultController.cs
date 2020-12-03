e4b6acdad19d38bac7101ead89fd2b6ffe9dd57a
Smthri <lockshin1999@mail.ru>
2020-12-03 18:02:17 +0300

    Task 4
    (cherry picked from commit d2101c243d7821e120c9c406f2dea714b5032cb7)

---
+++ WebLibrary/Controllers/ResultController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PredictorLibrary;
+
+namespace WebLibrary.Controllers
+{
+    [ApiController]
+    [Route("server")]
+    public class ResultController : ControllerBase
+    {
+        private Predictor _predictor;
+
+        [HttpGet("results")]
+        public Result[] GetResults()
+        {
+            return Predictor.ExtractDatabase();
+        }
+
+        [HttpPost]
+        public List<Result> ProcessDirectory([FromBody] string dir)
+        {
+            _predictor ??= new Predictor(dir, null);
+            _predictor.Stop();
+            _predictor.ImagePath = dir;
+            
+            _predictor.ProcessDirectory();
+
+            var res = Predictor.GetDatabaseDir(dir);
+            return res;
+        }
+
+        [HttpGet("dbstats")]
+        public string DBStats()
+        {
+            return Predictor.DatabaseStats();
+        }
+
+        [HttpDelete]
+        public void ClearDatabase()
+        {
+            _predictor?.Stop();
+            Predictor.ClearDatabase();
+        }
+    }
+}
